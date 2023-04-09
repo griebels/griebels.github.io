d8e891cd84b9c58b25a769bcb81a47fb7a91b54f
griebels <128572794+griebels@users.noreply.github.com>
2023-04-08 21:36:44 -0500

    adding JS, standardizing

---
+++ js/myJS.js
@@ -0,0 +1,17 @@
+function show_date_now() { 
+    let date1 = new Date();
+    alert("The date is " + date1)
+    }
+
+function time() {
+        var currentdate = new Date(); 
+        var timenow = + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+          document.getElementById("datebtn").innerHTML = "time is ="+timenow;
+        }
+
+function disappear_word() {
+    var theElement = document.getElementById("named");
+    theElement.innerHTML = ""
+}
